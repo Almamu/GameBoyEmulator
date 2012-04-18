1709014c22d4a3871b6ee29bad951116c1afb822
Almamu <games-fun-inc@hotmail.com>
2012-04-18 22:06:52 +0200

    Initial commit of the actual emulator status.
    Signed-off-by: Almamu <games-fun-inc@hotmail.com>

---
+++ Nintendo/GB/Screen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nintendo.GB
+{
+    public static class Screen
+    {
+        public enum ModeType
+        {
+            HBlank = 0,
+            VBlank = 1,
+            SearchingOamRam = 2,
+            TransferingData = 3
+        }
+
+        public const uint White = 0xFFFFFFFF;
+        public const uint LightGray = 0xFFAAAAAA;
+        public const uint DarkGray = 0xFF555555;
+        public const uint Black = 0xFF000000;
+
+        public static uint[] bgPallete = { White, LightGray, DarkGray, Black };
+        public static uint[] objPallete0 = { White, LightGray, DarkGray, Black };
+        public static uint[] objPallete1 = { White, LightGray, DarkGray, Black };
+
+        public static ModeType Mode;
+        public static bool CoincidenceInterruptEnabled;
+        public static bool OamInterruptEnabled;
+        public static bool VBlankInterruptEnabled;
+        public static bool HBlankInterruptEnabled;
+        public static bool InterruptRequested;
+        public static bool InterruptEnabled;
+        public static bool VBlankInterruptRequested;
+        public static bool HBlankInterruptRequested;
+        public static bool ControlOperationEnabled;
+        public static bool sprDisplayed;
+        public static bool bgDisplayed;
+        public static bool windowTileMapDisplaySelect;
+        public static bool windowDisplayed;
+        public static bool backgroundAndWindowTileDataSelect;
+        public static bool backgroundTileMapDisplaySelect;
+        public static bool largeSprites;
+
+        public static uint[,] bgBuffer = new uint[256, 256];
+        public static bool[,] bgTileInvalidated = new bool[32, 32];
+        public static bool invalidateAllBackgroundTilesRequest;
+        public static uint[, , ,] sprTile = new uint[256, 8, 8, 2];
+        public static bool[] sprTileInvalidated = new bool[256];
+        public static bool invalidateAllSpriteTilesRequest;
+        public static uint[,] windowBuffer = new uint[144, 168];
+
+        public static byte scrollX;
+        public static byte scrollY;
+        public static byte windowX;
+        public static byte windowY;
+        public static byte ly;
+        public static byte lyCompare;
+    }
+}
---
+++ Nintendo/GB/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nintendo.GB
+{
+    public static class Timer
+    {
+        public enum FrequencyType
+        {
+            hz4096 = 0,
+            hz262144 = 1,
+            hz65536 = 2,
+            hz16384 = 3
+        }
+
+        public static bool Running;
+        public static byte Counter;
+        public static byte Modulo;
+        public static byte Ticks;
+        public static FrequencyType Frequency;
+
+        public static bool OverflowInterruptRequested;
+        public static bool OverflowInterruptEnabled;
+
+        public static byte Seconds = 0;
+        public static byte Minutes = 0;
+        public static byte Hours = 0;
+        public static ushort Days = 0;
+
+        public static bool RTCEnabled = false;
+    }
+}
